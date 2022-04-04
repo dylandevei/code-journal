cc370cb87dd01fdb837f417a4187399036cfe39b
Dylan De Vei <dylan.devei@gmail.com>
2022-04-04 23:24:19 +0000

    In progress

--- js/main.js
+++ js/main.js
@@ -1,2 +1,26 @@
 /* global data */
 /* exported data */
+var $url = document.querySelector('.imageUrl');
+$url.addEventListener('input', handleInput);
+
+function handleInput(event) {
+  document.querySelector('img').src = $url;
+
+}
+
+// var $submit = document.querySelector('form');
+
+// $submit.addEventListener('submit', logSubmit);
+
+// function logSubmit(event) {
+//   var messageData = null;
+//   event.preventDefault();
+//   messageData = {
+//     title: document.forms[0].elements.title.value,
+//     imageUrl: document.forms[0].elements.imageUrl.value,
+//     notes: document.forms[0].elements.notes.value
+//   };
+//   console.log('messageData:', messageData);
+//   document.querySelector('form').reset();
+
+// }
