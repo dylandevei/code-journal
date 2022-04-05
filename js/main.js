f5f0adf8af91e7e5a539e70049f737f4362aa29a
Dylan De Vei <dylan.devei@gmail.com>
2022-04-05 20:23:13 +0000

    add dom tree to page and removed dummy data, steps 5-8, #2.

--- js/main.js
+++ js/main.js
@@ -28,3 +28,42 @@ function logSubmit(event) {
   $img.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
 }
+
+function renderEntry(entry) {
+  var title = entry.title;
+  var url = entry.imageUrl;
+  var note = entry.notes;
+
+  var $li = document.createElement('li');
+  var $columnHalf = document.createElement('div');
+  var $columnHalf2 = document.createElement('div');
+  var $entryImg = document.createElement('img');
+  var $h2 = document.createElement('h2');
+  var $p = document.createElement('p');
+
+  $li.setAttribute('class', 'row');
+  $columnHalf.setAttribute('class', 'column-half');
+  $columnHalf2.setAttribute('class', 'column-half');
+  $entryImg.setAttribute('src', url);
+  $h2.setAttribute('class', 'proza font');
+  $p.setAttribute('class', 'open-sans');
+
+  $h2.textContent = title;
+  $p.textContent = note;
+
+  $li.appendChild($columnHalf);
+  $li.append($columnHalf2);
+  $columnHalf.appendChild($entryImg);
+  $columnHalf2.appendChild($h2);
+  $columnHalf2.appendChild($p);
+
+  return $li;
+
+}
+
+var $ul = document.querySelector('.ul');
+
+for (var i = 0; i < data.entries.length; i++) {
+  var object = renderEntry(data.entries[i]);
+  $ul.appendChild(object);
+}
