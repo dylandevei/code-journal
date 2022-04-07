b8088ac67862ab8351567cdffd84dda6dd08dd1f
Dylan De Vei <dylan.devei@gmail.com>
2022-04-07 21:21:49 +0000

    Pre-populate the entry form with the clicked entry's values from the object found in the data model. #3

--- js/main.js
+++ js/main.js
@@ -11,14 +11,18 @@ var $form = document.querySelector('form');
 $form.addEventListener('submit', logSubmit);
 
 function logSubmit(event) {
-  var entry = null;
+  var entry = {};
   event.preventDefault();
-  entry = {
-    title: document.forms[0].elements.title.value,
-    imageUrl: document.forms[0].elements.imageUrl.value,
-    notes: document.forms[0].elements.notes.value,
-    entryId: data.nextEntryId
-  };
+  entry.title = $form.elements.title.value;
+  entry.imageUrl = $form.elements.imageUrl.value;
+  entry.notes = $form.elements.notes.value;
+  entry.entryId = data.nextEntryId;
+  // entry = {
+  //   title: $form.elements.title.value,
+  //   imageUrl: $form.elements.imageUrl.value,
+  //   notes: $form.elements.notes.value,
+  //   entryId: data.nextEntryId
+  // };
 
   data.nextEntryId++;
   data.entries.unshift(entry);
@@ -97,23 +101,33 @@ function handleClick(event) {
   switchViews(viewName);
 }
 
-function editClick(event) {
-  if (event.target && event.target.tagName === 'I') {
-    var closestUl = event.target.closest('.row');
-    var dataId = closestUl.getAttribute('data-entry-id');
-    switchViews('entry-form');
-    dataId = JSON.parse(dataId);
-    data.editing = dataId;
-
-  }
-}
-
 function switchViews(viewName) {
   for (var i = 0; i < $view.length; i++) {
     if ($view[i].getAttribute('data-view') === viewName) {
       $view[i].className = 'view';
     } else {
       $view[i].className = 'view hidden';
+    }
+  }
+}
+
+// var $title = document.querySelector('title');
+// var $photoUrl = document.querySelector('imageUrl');
+// var $notes = document.querySelector('notes');
+
+function editClick(event) {
+  if (event.target && event.target.tagName === 'I') {
+    var closestUl = event.target.closest('.row');
+    var dataId = closestUl.getAttribute('data-entry-id');
+    data.editing = JSON.parse(dataId);
+    for (var i = 0; i < data.entries.length; i++) {
+      if (data.editing === data.entries[i].entryId) {
+        switchViews('entry-form');
+        $form.elements.title.value = data.entries[i].title;
+        $form.elements.imageUrl.value = data.entries[i].imageUrl;
+        $form.elements.notes.value = data.entries[i].notes;
+        $img.setAttribute('src', data.entries[i].imageUrl);
+      }
     }
   }
 }
